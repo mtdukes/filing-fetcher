33ab10f522cae5b7f3fe06028256ab1c008a0c94
mtdukes <tyler.dukes@gmail.com>
2014-03-25 22:51:13 -0400

    adding functionality and gitignore

--- fetcher.py
+++ fetcher.py
@@ -1,18 +1,18 @@
-import urllib2
+import urllib2, urllib
 from bs4 import BeautifulSoup
 import re
+import argparse
 
-#read in a web page
-def get_response():
-   url_define = "https://stations.fcc.gov/station-profile/wral-tv/rss/feed-/political_file/2014"
-   response = urllib2.urlopen("https://stations.fcc.gov/station-profile/wral-tv/rss/feed-/political_file/2014")
+#read in a web page and station call letters defined by users
+#eventually would like to read in csv with these args predefined
+def get_response(url_define, station_define):
+   #url_define = "https://stations.fcc.gov/station-profile/wral-tv/rss/feed-/political_file/2014"
+   #station_define = "wral"
+   response = urllib2.urlopen(url_define)
    html = response.read()
    file_loc =[]
-   
-   #print html
 
    soup = BeautifulSoup(html)
-   #print(soup.prettify())
 
    #grab URLs for PDFs, starting with "collect"
    #have to deal with some weird RSS parsing, which is messing me up
@@ -24,8 +24,20 @@ def get_response():
    	file_loc.append(clean_url)
 
    for l in file_loc:
-   	print l
+      #check for spaces and replace with "%20"
+      pdf_url = urllib.quote("https://stations.fcc.gov//" + l, safe="%/:=&?~#+!$,;'@()*[]")
+      pdf_file = urllib2.urlopen(pdf_url)
+      #Use regex ^.*\/(.*) to grab filename
+      output = open(station_define+"/"+re.findall(r'^.*\/(.*)',l)[0],'wb')
+      output.write(pdf_file.read())
+      output.close()
+      print re.findall(r'^.*\/(.*)',l)[0]
+
 
 if __name__ == '__main__':
-	get_response()
-	print 'Done'
+   parser = argparse.ArgumentParser(description='Process the input')
+   parser.add_argument('path',help='Enter the station rss path')
+   parser.add_argument('call_sign',help='Enter the station call sign')
+   args = parser.parse_args()
+   get_response(args.path, args.call_sign)
+   print 'Done'
